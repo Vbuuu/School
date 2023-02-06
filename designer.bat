517552a75fbe6b094347a51bb4cb612b5034c47e
Vbuuu <m.rohm1@outlook.de>
2023-02-06 20:42:51 +0100

    -Removed Tkinter ui -Used Qt for frontend -Removed stuff I dont need -Added some scripts because I'm lazy as fuck -Added bad backend

---
+++ designer.bat
@@ -0,0 +1,5 @@
+if not exist .venv (
+  call setup.bat
+)
+
+call ".venv/Scripts/pyqt5-tools.exe" designer
--- src/backend.py
+++ src/backend.py
@@ -1,6 +1,6 @@
-
 class LoginManager:
     def login(username: str, password: str):
-        if username == "Max" and password == "Max": print("Logged In")
-        else: print("Invalid Credentials")
-
+        if username == "Max" and password == "Max":
+            print("Logged In")
+        else:
+            print("Invalid Credentials")
